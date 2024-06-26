5b0e0efaecdfb00ea121ecc7c5ca8b131d5d1ab9
saul201287 <84940926+saul201287@users.noreply.github.com>
2024-06-25 23:15:27 -0600

    Inicio de la creacion del sercidor y la conexion a la base de datos

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import morgan from "morgan";
+import { Signale } from "signale";
+import * as dotenv from "dotenv";
+import helmet from "helmet";
+import cors from "cors"
+import {query} from "./database/mysql"
+//import { userRouter } from "./user/infraestructura/UserRouter";
+
+const app = express();
+app.use(helmet.hidePoweredBy());
+dotenv.config();
+app.use(morgan("dev"));
+
+app.use(cors({ origin: "*" }));
+
+app.use(express.json());
+//app.use("/user", userRouter);
+
+const options = {
+  secrets: ["([0-9]{4}-?)+"],
+};
+
+const logger = new Signale(options);
+
+const port: string | undefined = process.env.PORT;
+
+app.listen(port, () => {
+  logger.success("server listening on port:", port);
+  //query("",[]);
+});
