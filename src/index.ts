a93d6655df0a80bfbcc5e6b327df666483f1751a
saul201287 <84940926+saul201287@users.noreply.github.com>
2024-07-14 09:21:59 -0600

    pruebas

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import { Signale } from "signale";
 import * as dotenv from "dotenv";
 import helmet from "helmet";
 import cors from "cors";
-//import cookieParser from 'cookie-parser';
 import rateLimit from "express-rate-limit";
 import https from 'https';
 import fs from "fs";
@@ -16,9 +15,7 @@ dotenv.config();
 const app = express();
 app.use(helmet.hidePoweredBy());
 app.use(morgan("dev"));
-
 app.use(cors({ origin: "*" }));
-
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
@@ -35,63 +32,14 @@ app.use("/user", userRouter);
 app.use("/plan", planRouter);
 app.use("/payments", paymentsRouter);
 
-const options = {
-  secrets: ["([0-9]{4}-?)+"],
-};
 const optionsHTTPS = {
   key: fs.readFileSync(String(process.env.RUTA_KEY)),
   cert: fs.readFileSync(String(process.env.RUTA_CERTIFICADO))
 };
-const logger = new Signale(options);
+const logger = new Signale();
 
-const port: string | undefined = process.env.PORT;
+const port = process.env.PORT;
 
 https.createServer(optionsHTTPS, app).listen(port, () => {
   logger.success("server listening on port:", port);
 });
-
-//Configuracion de HTTPS
-/*
-import express from "express";
-import morgan from "morgan";
-import { Signale } from "signale";
-import * as dotenv from "dotenv";
-import helmet from "helmet";
-import cors from "cors";
-import { userRouter } from "./user/infraestructure/RouterUser";
-import https from 'https';
-import fs from "fs";
-
-// Cargar variables de entorno
-dotenv.config();
-
-const app = express();
-
-// Configurar middlewares
-app.use(helmet.hidePoweredBy());
-app.use(morgan("dev"));
-app.use(cors({ origin: "*" }));
-app.use(express.json());
-
-// Configurar rutas
-app.use("/user", userRouter);
-
-// Configurar opciones HTTPS
-const optionsHTTPS = {
-  key: fs.readFileSync(String(process.env.RUTA_KEY)),
-  cert: fs.readFileSync(String(process.env.RUTA_CERTIFICADO))
-};
-
-const options = {
-  secrets: ["([0-9]{4}-?)+"],
-};
-
-const logger = new Signale(options);
-
-const port: string | undefined = process.env.PORT;
-
-// Crear y lanzar el servidor HTTPS
-https.createServer(optionsHTTPS, app).listen(port, () => {
-  logger.success("server listening on port:", port);
-});
-*/
