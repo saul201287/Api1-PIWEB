5484d17366fa62fd34a724a3214d9145e6524d2b
saul201287 <84940926+saul201287@users.noreply.github.com>
2024-07-17 17:10:47 -0600

    Mas seguridad

--- src/index.ts
+++ src/index.ts
@@ -57,29 +57,48 @@ import * as dotenv from "dotenv";
 import helmet from "helmet";
 import cors from "cors";
 import rateLimit from "express-rate-limit";
-import https from 'https';
+import https from "https";
 import fs from "fs";
 import { userRouter } from "./user/infraestructure/RouterUser";
 import { planRouter } from "./planes/infraestructure/RoouterPlans";
 import { paymentsRouter } from "./payments/infraestructure/PaymentsRouter";
 import { routerProduct } from "./product/infraestructure/RouterProduct";
 
+const corsOptions = {
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
 dotenv.config();
 const app = express();
 app.use(helmet.hidePoweredBy());
 app.use(morgan("dev"));
-app.use(cors({ origin: "*" }));
+app.use(cors(corsOptions));
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
   keyGenerator: (req, res) => {
-    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    return ip ? ip.toString() : 'default';
-  }
+    const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+    return ip ? ip.toString() : "default";
+  },
 });
+
 app.use((req, res, next) => {
-  res.setHeader("Content-Security-Policy", "default-src 'self'; script-src 'self' https://code.jquery.com https://stackpath.bootstrapcdn.com; style-src 'self' https://stackpath.bootstrapcdn.com; img-src 'self' data:; font-src 'self' https://stackpath.bootstrapcdn.com; object-src 'none';");
+  const forbiddenPaths = ["/autodiscover/autodiscover.json", "/Powershell"];
+  if (forbiddenPaths.some((path) => req.path.includes(path))) {
+    return res.status(403).send("Forbidden");
+  }
+  next();
+});
+
+app.use((req, res, next) => {
+  res.setHeader(
+    "Content-Security-Policy",
+    "default-src 'self'; script-src 'self' https://code.jquery.com https://stackpath.bootstrapcdn.com; style-src 'self' https://stackpath.bootstrapcdn.com; img-src 'self' data:; font-src 'self' https://stackpath.bootstrapcdn.com; object-src 'none';"
+  );
   res.setHeader("X-Frame-Options", "DENY");
   res.setHeader("X-Content-Type-Options", "nosniff");
   res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
@@ -93,7 +112,7 @@ app.use(express.json());
 app.use("/user", userRouter);
 app.use("/plan", planRouter);
 app.use("/payments", paymentsRouter);
-app.use("/product",routerProduct)
+app.use("/product", routerProduct);
 
 app.get("/", (req, res) => {
   res.send("API is running");
@@ -104,7 +123,7 @@ const options = {
 
 const optionsHTTPS = {
   key: fs.readFileSync(String(process.env.RUTA_KEY)),
-  cert: fs.readFileSync(String(process.env.RUTA_CERTIFICADO))
+  cert: fs.readFileSync(String(process.env.RUTA_CERTIFICADO)),
 };
 
 const logger = new Signale(options);
