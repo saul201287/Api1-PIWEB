b49914c244bec6dcd32b90be9b6e9673541b71e8
saul201287 <84940926+saul201287@users.noreply.github.com>
2024-07-21 20:09:45 -0600

    https

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-
+/*
 import express from "express";
 import morgan from "morgan";
 import { Signale } from "signale";
@@ -48,8 +48,8 @@ const port = process.env.PORT;
 app.listen(port, () => {
   logger.success("server listening on port:", port);
 });
+*/
 
-/*
 import express from "express";
 import morgan from "morgan";
 import { Signale } from "signale";
@@ -124,4 +124,3 @@ const port = process.env.PORT;
 https.createServer(optionsHTTPS, app).listen(port, () => {
   logger.success("server listening on port:", port);
 });
-*/
