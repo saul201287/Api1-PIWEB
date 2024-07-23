596de5fd26f10cb9e9e9e971802c0b1ef8f2a737
saul201287 <84940926+saul201287@users.noreply.github.com>
2024-07-23 03:17:34 -0600

    pasar es pasar

--- src/index.ts
+++ src/index.ts
@@ -49,7 +49,6 @@ app.listen(port, () => {
   logger.success("server listening on port:", port);
 });
 */
-
 import express from "express";
 import morgan from "morgan";
 import { Signale } from "signale";
@@ -65,26 +64,32 @@ import { paymentsRouter } from "./payments/infraestructure/PaymentsRouter";
 import { routerProduct } from "./product/infraestructure/RouterProduct";
 import { routerData } from "./datas/infraestructure/RouterDatas";
 
+dotenv.config();
+
 const corsOptions = {
-  origin:  ['https://www.powerwatch.piweb.lat', 'https://powerwatch.piweb.lat'],
+  origin: ['https://www.powerwatch.piweb.lat', 'https://powerwatch.piweb.lat'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  optionsSuccessStatus: 200 
 };
 
-dotenv.config();
 const app = express();
+
 app.use(helmet.hidePoweredBy());
 app.use(morgan("dev"));
 app.use(cors(corsOptions));
+app.use(express.json());
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
-  keyGenerator: (req, res) => {
+  keyGenerator: (req) => {
     const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
     return ip ? ip.toString() : "default";
   },
 });
+
+app.use(limiter);
 
 app.use((req, res, next) => {
   const forbiddenPaths = ["/autodiscover/autodiscover.json", "/Powershell"];
@@ -94,32 +99,27 @@ app.use((req, res, next) => {
   next();
 });
 
-
-
-app.use(limiter);
-app.use(express.json());
 app.use("/user", userRouter);
 app.use("/plan", planRouter);
 app.use("/payments", paymentsRouter);
 app.use("/product", routerProduct);
-app.use("/data",routerData)
+app.use("/data", routerData);
 
 app.get("/", (req, res) => {
   res.send("API is running");
 });
-const options = {
-  secrets: ["([0-9]{4}-?)+"],
-};
 
 const optionsHTTPS = {
   key: fs.readFileSync(String(process.env.RUTA_KEY)),
   cert: fs.readFileSync(String(process.env.RUTA_CERTIFICADO)),
 };
 
-const logger = new Signale(options);
+const logger = new Signale({
+  secrets: ["([0-9]{4}-?)+"],
+});
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 https.createServer(optionsHTTPS, app).listen(port, () => {
-  logger.success("server listening on port:", port);
+  logger.success("Server listening on port:", port);
 });
